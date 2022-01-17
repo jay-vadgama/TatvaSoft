d8e5f3aeaf5dc2f3fb6734069dfa1934ca569739
LAPTOP-8UV75MH8\Jay <jayvadgama143@gmail.com>
2022-01-17 19:01:04 +0530

    Database Excel Sheet Added.

--- HTML/JS/main.js
+++ HTML/JS/main.js
@@ -1,3 +1,13 @@
 $(window).scroll(function(){
     $('navbar').toggleClass('scrolled',$(this).scrollTop()>60);
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      toTop.classList.add("active");
+    } else {
+      toTop.classList.remove("active");
+    }
+  })
