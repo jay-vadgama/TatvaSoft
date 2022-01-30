fad5f9bf1512dd632f4d9da4aeda382c5ccc223f
LAPTOP-8UV75MH8\Jay <jayvadgama143@gmail.com>
2022-01-30 17:33:34 +0530

    Files Updated

--- HTML/JS/main.js
+++ HTML/JS/main.js
@@ -1,18 +1,4 @@
-$(window).scroll(function(){
-    $('navbar').toggleClass('scrolled',$(this).scrollTop()>60);
-});
-
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-      toTop.classList.add("active");
-    } else {
-      toTop.classList.remove("active");
-    }
-  })
-
-  function footerPolicy(){
+function footerPolicy(){
     document.getElementById("privacyPolicy").style.display="none";
     document.getElementById("mainFooter").style.marginBottom="0px";
   }
